0e569b7c5d1d75b64cc58b43951699b114ad48d8
Rennason <rennason@gmail.com>
2021-02-25 14:49:04 -0300

    Melhoria no cálculo de calorias e melhorias gerais na interface

--- app/src/main/java/com/sdeoliveira/maps/services/TrackingService.kt
+++ app/src/main/java/com/sdeoliveira/maps/services/TrackingService.kt
@@ -49,6 +49,11 @@ class TrackingService : LifecycleService() {
         timeRunMiliSeconds.postValue(0L)
     }
 
+    public fun init2Values(pIsTracking: Boolean)
+    {
+        //isTracking.postValue(is)
+    }
+
     override fun onCreate() {
         super.onCreate()
         initValues()
